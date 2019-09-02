8ca0cc5d2e5d185700403c7a5b98c88c72a93862
s3718861 <s3718861@student.rmit.edu.au>
2019-09-02 10:08:57 +1000

    Created new directory and uploaded todo jsx

---
+++ frontend/todo-app/src/components/Todo2/AuthenticationService.js
@@ -0,0 +1,16 @@
+class AuthenticationService {
+    registerSuccessfulLogin(username, password) {
+        console.log('register successful login');
+        sessionStorage.setItem('authenticatedUser', username);
+    }
+    logout() {
+        sessionStorage.removeItem('authenticatedUser');
+    }
+    isUserLoggedIn() {
+        let user = sessionStorage.getItem('authenticatedUser')
+        if(user === null) return false
+        return true
+    }
+}
+
+export default new AuthenticationService()

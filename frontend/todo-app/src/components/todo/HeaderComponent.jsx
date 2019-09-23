273a902457ba549eb8d8b354e60e21ac9b6cc0b4
s3718861 <s3718861@student.rmit.edu.au>
2019-09-23 14:06:36 +1000

    Added nav bar for maps api

--- frontend/todo-app/src/components/todo/HeaderComponent.jsx
+++ frontend/todo-app/src/components/todo/HeaderComponent.jsx
@@ -14,6 +14,7 @@ class HeaderComponent extends Component{
                     <ul className="navbar-nav">
                         {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/archmod">Home</Link></li>}
                         {isUserLoggedIn && <li><Link className="nav-link" to="/todos">Todos</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/map-api">Map API</Link></li>}
                     </ul>
                     <ul className="navbar-nasv navbar-collapse justify-content-end">
                         {!isUserLoggedIn && <li><Link className="nav-link" to="/login">Login</Link></li>}

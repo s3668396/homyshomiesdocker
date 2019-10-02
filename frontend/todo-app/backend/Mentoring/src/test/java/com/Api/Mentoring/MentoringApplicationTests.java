b44c04464f6dee5bebcc747bc80f8d7814c28b28
joel12 <joeltjacob@outlook.com>
2019-10-03 01:30:59 +1000

    Backend
    backend for Mentoring api

---
+++ frontend/todo-app/backend/Mentoring/src/test/java/com/Api/Mentoring/MentoringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Api.Mentoring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MentoringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
